a0d471ef09fad36813d5585c46a1c92072096c6d
limsion <sion1212@naver.com>
2023-03-03 11:44:16 +0900

    Changes

---
+++ src/main/java/com/fastcampus/ch4/controller/BoardController.java
@@ -0,0 +1,155 @@
+package com.fastcampus.ch4.controller;
+
+import com.fastcampus.ch4.domain.BoardDto;
+import com.fastcampus.ch4.domain.PageHandler;
+import com.fastcampus.ch4.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    @Autowired
+    BoardService boardService;
+
+
+    // 게시물 수정
+    @PostMapping("/modify")
+    public String modify(BoardDto boardDto, Model m, HttpSession session, RedirectAttributes rattr) {
+        String writer = (String) session.getAttribute("id");
+        boardDto.setWriter(writer);
+
+        try {
+            int rowCnt = boardService.modify(boardDto);
+
+            if (rowCnt!= 1)
+                throw new Exception("modify failed");
+            rattr.addFlashAttribute("msg", "MOD_OK");
+
+            return "redirect:/board/list";
+        } catch (Exception e) {
+            e.printStackTrace();
+            m.addAttribute(boardDto);
+            rattr.addFlashAttribute("msg", "MOD_ERR");
+            return "board";
+        }
+
+    }
+
+// 게시물 쓰기
+    @PostMapping("/write")
+    public String write(BoardDto boardDto, Model m, HttpSession session, RedirectAttributes rattr) {
+        String writer = (String) session.getAttribute("id");
+        boardDto.setWriter(writer);
+
+        try {
+            int rowCnt = boardService.write(boardDto);
+
+            if (rowCnt!= 1)
+                throw new Exception("Write failed");
+            rattr.addFlashAttribute("msg", "WRT_OK");
+
+            return "redirect:/board/list";
+        } catch (Exception e) {
+           e.printStackTrace();
+           m.addAttribute(boardDto);
+           rattr.addFlashAttribute("msg", "WRT_ERR");
+            return "board";
+        }
+
+    }
+
+    @GetMapping("/write")
+    public  String write(Model m) {
+        m.addAttribute("mode", "new");
+        return "board"; //읽기와 쓰기에 사용 쓰기에 사용할때는 mode=new
+    }
+
+    @PostMapping("remove")
+    public String remove(Integer bno, Integer page, Integer pageSize, Model m, HttpSession session, RedirectAttributes rattr) {
+        String writer = (String)session.getAttribute("id");
+        try {
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+
+            int rowCnt = boardService.remove(bno, writer);
+
+            if (rowCnt!=1)
+                    throw new Exception("board remove error");
+
+                // RedirectAttributes rattr (addFlashAttribute) 잠시 세션에서 사용하고 플레시 사라짐
+                rattr.addFlashAttribute("msg", "DEL_OK");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+             rattr.addFlashAttribute("msg", "DEL_ERR");
+        }
+
+
+        return "redirect:/board/list";
+    }
+
+
+    @GetMapping("/read")
+    public String read(Integer bno, Integer page, Integer pageSize, Model m) {
+        try {
+            BoardDto boardDto = boardService.read(bno);
+//            m.addAttribute("boardDto", boardDto);  아래문장과 동일
+            m.addAttribute(boardDto);
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return  "board";
+    }
+
+    @GetMapping("/list")
+    public String list(Integer page, Integer pageSize, Model m, HttpServletRequest request) {
+        if(!loginCheck(request))
+            return "redirect:/login/login?toURL="+request.getRequestURL();  // 로그인을 안했으면 로그인 화면으로 이동
+
+        if (page ==null) page= 1;
+        if (pageSize == null) pageSize=10;
+
+
+        try {
+
+            int totalCnt = boardService.getCount();
+            PageHandler pageHandler = new PageHandler(totalCnt, page, pageSize);
+
+            Map map = new HashMap();
+            map.put("offset" , ( page -1 ) * pageSize);
+            map.put("pageSize", pageSize);
+
+            List<BoardDto> list = boardService.getPage(map);
+            m.addAttribute("list", list);
+            m.addAttribute("ph", pageHandler);
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize" , pageSize);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return "boardList"; // 로그인을 한 상태이면, 게시판 화면으로 이동
+    }
+
+    private boolean loginCheck(HttpServletRequest request) {
+        // 1. 세션을 얻어서
+        HttpSession session = request.getSession();
+        // 2. 세션에 id가 있는지 확인, 있으면 true를 반환
+        return session.getAttribute("id")!=null;
+    }
+}
+
---
+++ target/ch4-1.0.0-BUILD-SNAPSHOT/WEB-INF/views/board.jsp
@@ -0,0 +1,145 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>fastcampus</title>
+    <link rel="stylesheet" href="<c:url value='/css/menu.css'/>">
+    <script src="https://code.jquery.com/jquery-1.11.3.js"></script>
+
+    <style>
+        * {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+            font-family: "Noto Sans KR", sans-serif;
+        }
+        .container {
+            width : 50%;
+            margin : auto;
+        }
+        .writing-header {
+            position: relative;
+            margin: 20px 0 0 0;
+            padding-bottom: 10px;
+            border-bottom: 1px solid #323232;
+        }
+        input {
+            width: 100%;
+            height: 35px;
+            margin: 5px 0px 10px 0px;
+            border: 1px solid #e9e8e8;
+            padding: 8px;
+            background: #f8f8f8;
+            outline-color: #e6e6e6;
+        }
+        textarea {
+            width: 100%;
+            background: #f8f8f8;
+            margin: 5px 0px 10px 0px;
+            border: 1px solid #e9e8e8;
+            resize: none;
+            padding: 8px;
+            outline-color: #e6e6e6;
+        }
+        .frm {
+            width:100%;
+        }
+        .btn {
+            background-color: rgb(236, 236, 236); /* Blue background */
+            border: none; /* Remove borders */
+            color: black; /* White text */
+            padding: 6px 12px; /* Some padding */
+            font-size: 16px; /* Set a font size */
+            cursor: pointer; /* Mouse pointer on hover */
+            border-radius: 5px;
+        }
+        .btn:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+<div id="menu">
+    <ul>
+        <li id="logo">fastcampus</li>
+        <li><a href="<c:url value='/'/>">Home</a></li>
+        <li><a href="<c:url value='/board/list'/>">Board</a></li>
+        <li><a href="<c:url value='/login/login'/>">login</a></li>
+        <li><a href="<c:url value='/register/add'/>">Sign in</a></li>
+        <li><a href=""><i class="fas fa-search small"></i></a></li>
+    </ul>
+</div>
+    <script>
+        let msg="${msg}";
+        if(msg=="WRT_ERR") alert("게시물 등록이 실패하였습니다 다시 시도해주세요");
+        if(msg=="MOD_ERR") alert("게시물 수정이 실패하였습니다 다시 시도해주세요");
+
+    </script>
+<div style="text-align:center">
+    <h2>게시물 ${mode=="new" ? "글쓰기" : "읽기"}</h2>
+
+    <form action="" id="form">
+        <input type="hidden" name="bno" value="${boardDto.bno}">
+        <input type="text" name="title" value="${boardDto.title}" ${ mode == "new" ? '' : "readonly='readonly'"}>
+        <textarea name="content" id="" cols="30" rows="10" ${ mode == "new" ? '' : "readonly='readonly'"}> ${boardDto.content}</textarea>
+        <button type="button" id="writeBtn" class="btn" ${mode == "new" ? "show" : "hidden" }>글쓰기</button>
+        <button type="button" id="modifyBtn" class="btn" ${mode == "new" ? "hidden" : "show" }>수정</button>
+        <button type="button" id="removeBtn" class="btn" ${mode == "new" ? "hidden" : "show" }>삭제</button>
+        <button type="button" id="listBtn" class="btn">목록</button>
+    </form>
+</div>
+<script>
+<%--    목록 버튼 제이쿼리--%>
+    $(document).ready(function () {
+        $('#listBtn').on("click", function () {
+            location.href = "<c:url value='/board/list'/>?page=${page}&pageSize=${pageSize}";
+        })
+
+    // 삭제 버튼
+
+        $('#removeBtn').on("click", function () {
+            if (! confirm("정말로 삭제하시겠습니까?")) return;
+            let form = $('form');
+            form.attr("action", "<c:url value='/board/remove'/>?page=${page}&pageSize=${pageSize}");
+            form.attr("method", "post");
+            form.submit();
+        })
+
+    // 쓰기버튼
+
+        $('#writeBtn').on("click", function () {
+            let form = $('#form');
+            form.attr("action", "<c:url value='/board/write'/>");
+            form.attr("method", "post");
+            form.submit();
+        })
+
+    // 수정 버튼
+
+        $('#modifyBtn').on("click", function () {
+            // 1. 읽기 상태이면 수정 상태로 변경
+            let form = $("#form");
+            let isReadOnly = $("input[name=title]").attr('readonly');
+
+            if (isReadOnly != null) {
+                $("input[name=title]").attr('readonly', false);   // 제목
+                $("textarea").attr('readonly', false);          // 내용
+                $("#modifyBtn").html("등록");               //수정버튼 등록으로 변경
+                $("#writeBtn").hide();
+                $("h2").html("게시물 수정");
+                return;
+            }
+
+            // 2. 수정 상태이면 수정된 내용을 서버로 전송
+            form.attr("action", "<c:url value='/board/modify'/>");
+            form.attr("method", "post");
+
+                  form.submit();
+        })
+    });
+</script>
+</body>
+</html>
